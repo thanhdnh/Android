6f56c9af98a87bcd7eb03906fdb26152ed103f17
thanhdnh <myhoangthanh@yahoo.com>
2023-08-09 10:12:04 +0700

    OK

---
+++ app/src/main/java/com/example/javaproject/ISystem.java
@@ -0,0 +1,6 @@
+package com.example.javaproject;
+
+public interface ISystem {
+  public void Store();
+  public void Backup();
+}
---
+++ app/src/main/java/com/example/javaproject/MySystem.java
@@ -0,0 +1,13 @@
+package com.example.javaproject;
+
+public class MySystem implements ISystem{
+  @Override
+  public void Store() {
+
+  }
+
+  @Override
+  public void Backup() {
+
+  }
+}
